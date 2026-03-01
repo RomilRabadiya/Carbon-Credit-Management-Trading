ec95286beb49a327681af8264e3b62e7c7fdbbec
vrund patel <vrundp1811@gmail.com>
2026-03-01 10:57:12 +0530

    Add trade service

---
+++ carbon-credits-system/trade-service/src/main/java/com/carbon/trade/TradeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.carbon.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableFeignClients
+public class TradeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeServiceApplication.class, args);
+    }
+}
